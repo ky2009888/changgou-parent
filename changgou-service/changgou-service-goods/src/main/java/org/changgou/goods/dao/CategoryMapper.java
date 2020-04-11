7d4d57c298567960af32c13d71a50977ece168ba
cf_jijinliang <ji_jinliang2012@163.com>
2020-04-11 16:19:35 +0800

    电商项目之商品相关的表的服务接口

--- changgou-service/changgou-service-goods/src/main/java/org/changgou/goods/dao/CategoryMapper.java
+++ changgou-service/changgou-service-goods/src/main/java/org/changgou/goods/dao/CategoryMapper.java
@@ -1,6 +1,10 @@
 package org.changgou.goods.dao;
+
+import org.apache.ibatis.annotations.Select;
 import org.changgou.goods.pojo.Category;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 /****
  * @Author:shenkunlin
@@ -8,4 +12,12 @@ import tk.mybatis.mapper.common.Mapper;
  * @Date 2019/6/14 0:12
  *****/
 public interface CategoryMapper extends Mapper<Category> {
+    /**
+     * 根据parentId查询所有的子分类
+     *
+     * @param parentId 父分类ID
+     * @return List<Category>
+     */
+    @Select(" select * from tb_category t where t.parent_id = #{parentId} ")
+    List<Category> findByParentId(Integer parentId);
 }
