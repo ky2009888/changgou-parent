15bc2faf1466e69ea64665efa0c8bf2d2faf831e
cf_jijinliang <ji_jinliang2012@163.com>
2020-04-28 12:30:08 +0800

    新增SpringCloudGateWay之限流操作

--- changgou-gateway/changgou-gateway-web/src/main/java/org/changgou/gateway/GateWayWebApplication.java
+++ changgou-gateway/changgou-gateway-web/src/main/java/org/changgou/gateway/GateWayWebApplication.java
@@ -1,8 +1,13 @@
 package org.changgou.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 /**
  * 功能描述:
@@ -13,8 +18,26 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@Slf4j
 public class GateWayWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GateWayWebApplication.class,args);
+        SpringApplication.run(GateWayWebApplication.class, args);
+    }
+
+    /**
+     * 获取用户的唯一的KEY进行限流操作
+     *
+     * @return KeyResolver
+     */
+    @Bean(name = "ipKeyResolver")
+    public KeyResolver userKeyResolver() {
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                String ip = exchange.getRequest().getRemoteAddress().getHostString();
+                log.info("userIp:{}", ip);
+                return Mono.just(ip);
+            }
+        };
     }
 }
