abc452ed3721bc77c37af766e1542235bceffb46
cf_jijinliang <ji_jinliang2012@163.com>
2020-05-05 23:02:15 +0800

    订单微服务

--- changgou-common/src/main/java/com/changgou/utils/ConvertUtils.java
+++ changgou-common/src/main/java/com/changgou/utils/ConvertUtils.java
@@ -1,8 +1,11 @@
 package com.changgou.utils;
 
+import cn.hutool.core.io.IoUtil;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 转换工具类
@@ -11,6 +14,7 @@ public class ConvertUtils {
 
     /**
      * 输入流转换为xml字符串
+     *
      * @param inputStream
      * @return
      */
@@ -23,7 +27,20 @@ public class ConvertUtils {
         }
         outSteam.close();
         inputStream.close();
-        String result  = new String(outSteam.toByteArray(), "utf-8");
+        String result = new String(outSteam.toByteArray(), StandardCharsets.UTF_8);
+        return result;
+    }
+
+    /**
+     * 读取流中的内容
+     *
+     * @param inputStream 输入流
+     * @return String
+     * @throws IOException
+     */
+    public static String convertToStringByIoUtils(InputStream inputStream) throws IOException {
+        String result = IoUtil.read(inputStream, "utf-8");
+        inputStream.close();
         return result;
     }
 }
